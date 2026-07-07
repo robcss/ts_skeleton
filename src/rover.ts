839c97b40baf90c8068401726b998cb647043c1b
robcss <77405130+robcss@users.noreply.github.com>
2026-07-07 17:24:08 +0200

    Add turn left test

--- src/rover.ts
+++ src/rover.ts
@@ -1,13 +1,33 @@
-// type Heading = 'N';s
+export type Heading = 'N' | 'E' | 'S' | 'W'
 type Position = { x: number, y: number };
 
 class Rover {
 
     public position: Position;
     public heading: string;
-    constructor(position: Position, heading: string) {
-        this.position = {x: 2, y: 3};
-        this.heading = 'N';
+    constructor(position: Position, heading: Heading) {
+        this.position = { x: 2, y: 3 };
+        this.heading = heading;
+    }
+
+    move(command: string): void {
+
+        switch (this.heading) {
+            case 'N':
+                this.heading = 'W'
+                break;
+            case 'E':
+                this.heading = 'N'
+                break;
+            case 'S':
+                this.heading = 'E'
+                break;
+            case 'W':
+                this.heading = 'S'
+                break;
+            default:
+                break;
+        }
     }
 }
 
---
+++ test/turn_left.test.ts
@@ -0,0 +1,38 @@
+import { strict as assert } from "assert";
+import { describe, it } from "node:test";
+import Rover, { Heading } from "../src/rover";
+
+describe('Turn left', () => {
+
+    it('', () => {
+
+        const directions: {start: Heading, end: Heading}[] = [{
+            start: 'N',
+            end: 'W'
+        },
+        {
+            start: 'W',
+            end: 'S'
+        },
+        {
+            start: 'S',
+            end: 'E'
+        },
+        {
+            start: 'E',
+            end: 'N'
+        },
+    ]
+
+        for (const {start, end} of directions) {
+            const rover = new Rover({ x: 0, y: 0 }, start)
+
+            rover.move('L')
+    
+            assert.equal(rover.heading, end)
+        }
+
+    })
+
+
+})
