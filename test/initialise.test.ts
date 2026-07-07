c9b533362bff6ebbb2a9411c2590eaa04e94b474
robcss <77405130+robcss@users.noreply.github.com>
2026-07-07 17:29:44 +0200

    Refactor initialise test in separate tests

--- test/initialise.test.ts
+++ test/initialise.test.ts
@@ -5,14 +5,18 @@ import Rover from "../src/rover";
 describe("initialise", () => {
 
 
-    it('', () => {
+    it('When is initialized with position (2,3), rover position is (2,3)', () => {
 
         const rover = new Rover({x: 2, y: 3}, 'N');
         assert.equal(rover.position.x, 2);
         assert.equal(rover.position.y, 3);
-        assert.equal(rover.heading, 'N');
     })
 
+    it('When is initialized with heading N, rover position is N', () => {
+
+        const rover = new Rover({x: 2, y: 3}, 'N');
+        assert.equal(rover.heading, 'N');
+    })
 
 }
 )
